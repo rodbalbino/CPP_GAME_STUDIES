202e7bcae25517cc02da2ca63535092166368866
Administrator <admin@digitalwolk.com>
2025-03-27 15:18:47 -0300

    Initial Commit

---
+++ Reactions/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Reactions
+//
+//  Created by Rodrigo Balbino on 3/27/25.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
