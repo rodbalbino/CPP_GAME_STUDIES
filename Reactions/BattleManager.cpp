4c78411105f55c394557b2c7b5d7c4c23107845b
Administrator <admin@digitalwolk.com>
2025-05-05 22:01:09 -0300

    Add Battle Manager

---
+++ Reactions/BattleManager.cpp
@@ -0,0 +1,8 @@
+//
+//  BattleManager.cpp
+//  Reactions
+//
+//  Created by Rodrigo Balbino on 5/5/25.
+//
+
+#include "BattleManager.hpp"
---
+++ Reactions/BattleManager.hpp
@@ -0,0 +1,13 @@
+//
+//  BattleManager.hpp
+//  Reactions
+//
+//  Created by Rodrigo Balbino on 5/5/25.
+//
+
+#ifndef BattleManager_hpp
+#define BattleManager_hpp
+
+#include <stdio.h>
+
+#endif /* BattleManager_hpp */
